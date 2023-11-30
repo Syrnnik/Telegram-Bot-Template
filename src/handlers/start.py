53eb0698a3cf5d2bd0b0bfbd0d78e81b11344667
Syrnnik <sakh.alexandr@gmail.com>
2023-12-01 01:42:10 +0300

    fix: start command handler

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -1,14 +1,15 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
+from constants.bot_commands import start_command
 from states.example import ExampleState
 
 start_router = Router(name=__name__)
 
 
-@start_router.message(CommandStart())
+@start_router.message(Command(start_command))
 async def start(message: Message, state: FSMContext):
     # Set user state
     await state.set_state(ExampleState.example)
