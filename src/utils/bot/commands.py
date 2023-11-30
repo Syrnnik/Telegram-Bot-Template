dd748394bc1f04376fcff529d2729c0f1637f3b2
Syrnnik <sakh.alexandr@gmail.com>
2023-11-30 20:07:50 +0300

    feat: add utils with bot funcs

---
+++ src/utils/bot/commands.py
@@ -0,0 +1,9 @@
+from aiogram import Bot
+
+
+async def set_bot_commands(bot: Bot):
+    # await bot.set_my_commands([
+    #     your-command-1,
+    #     your-command-2
+    # ])
+    pass
---
+++ src/utils/bot/routers.py
@@ -0,0 +1,10 @@
+from aiogram import Dispatcher
+
+
+def set_bot_routers(dispatcher: Dispatcher):
+    # Example of including router
+    # dispatcher.include_routers(
+    #     your-router-1,
+    #     your-router-2
+    # )
+    pass
