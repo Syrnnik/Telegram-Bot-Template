23c333c1d91997e577cc19f5e553cb710729e393
Syrnnik <sakh.alexandr@gmail.com>
2024-04-17 01:14:22 +0300

    fix: update routers and start handler

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -4,6 +4,8 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
 from constants.bot_commands import start_command
+from keyboards.inline.example import example_inline_keyboard
+from keyboards.reply.example import example_reply_keyboard
 from states.example import ExampleState
 
 start_router = Router(name=__name__)
@@ -14,4 +16,7 @@ async def start(message: Message, state: FSMContext):
     # Set user state
     await state.set_state(ExampleState.example)
     # Answer to user message
-    await message.answer("Hello!")
+    # With inline keyboard
+    await message.answer("Hello!", reply_markup=example_inline_keyboard)
+    # With reply keyboard
+    await message.answer("I am bot..", reply_markup=example_reply_keyboard)
--- src/utils/bot/routers.py
+++ src/utils/bot/routers.py
@@ -1,9 +1,13 @@
 from aiogram import Dispatcher
 
+from callbacks.example import start_callback_router
+from handlers.button import button_router
 from handlers.start import start_router
 
 
 def set_bot_routers(dispatcher: Dispatcher):
     dispatcher.include_routers(
         start_router,
+        start_callback_router,
+        button_router,
     )
